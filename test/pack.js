03976c2f678903c70d5c7feb547ab23d3975fe3b
finnp <derfinn@gmail.com>
2015-12-15 00:23:44 +0100

    resolve error with odd file names

--- test/pack.js
+++ test/pack.js
@@ -51,4 +51,48 @@ module.exports = function (test) {
       t.deepEqual(data, expected)
     }))
   })
+
+  test('odc: directories and file', function (t) {
+    t.plan(4)
+
+    var pack = cpio.pack()
+
+    pack.entry({
+      name: '.',
+      mtime: new Date(1419354218000),
+      mode: 16877,
+      uid: 501,
+      gid: 20
+    }, function (err) {
+      t.ok(!err, 'err dir .')
+      pack.entry({
+        name: './a',
+        mtime: new Date(1419354218000),
+        mode: 16877,
+        uid: 501,
+        gid: 20
+      }, function (err) {
+        t.ok(!err, 'err dir ./')
+        var entry = pack.entry({
+          name: './a/test.txt',
+          mode: 33188,
+          mtime: new Date(1450121663000),
+          size: 5,
+          uid: 501,
+          gid: 20
+        }, function (err) {
+          t.ok(!err, 'err file')
+          pack.finalize()
+        })
+
+        entry.write('test\n')
+        entry.end()
+      })
+    })
+
+    pack.pipe(concat(function (data) {
+      // TODO: test contents
+      t.ok(true, 'complete')
+    }))
+  })
 }
